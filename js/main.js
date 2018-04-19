bb3699054c3dc1c1aa0db5d8722fe69f1f1d86fa
Gabe Julian <gabrialjulian@gmail.com>
2018-04-19 16:20:51 -0400

    Fixed Site

--- js/main.js
+++ js/main.js
@@ -1,57 +1,62 @@
-alert("Currently Broken");
+
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 
 function saveBookmark(e){
-    let siteName = document.getElementById('siteName').value;
-    let siteUrl = document.getElementById('siteUrl').value;
+  var siteName =document.getElementById('siteName').value;
+  var siteUrl =document.getElementById('siteUrl').value;
 
-    let bookmark = {
-        name: siteName,
-        url: siteUrl
-    }
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
 
-    if(localStorage.getItem('bookmarks') === undefined){
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
 
-        let bookmarks = [];
 
-        bookmarks.push(bookmark);
+  if(localStorage.getItem('bookmarks') === null){
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);6
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
 
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    } else {
-        let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  document.getElementById('myForm').reset();
 
-        bookmarks.push(bookmark);
+  fetchBookmarks();
 
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }
-
-    
-    e.preventDefault();
+  
+  e.preventDefault();
 }
 
 
 function deleteBookmark(url){
-
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
-    for(var i =0;i < bookmarks.length;i++){
-      if(bookmarks[i].url == url){
-
-        bookmarks.splice(i, 1);
-      }
+  
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+   
+      bookmarks.splice(i, 1);
     }
-
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-
-    fetchBookmarks();
   }
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+
+  fetchBookmarks();
+}
+
 
 function fetchBookmarks(){
-    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
-    let bookmarksResults = document.getElementById('bookmarksResults');
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
+  var bookmarksResults = document.getElementById('bookmarksResults');
 
   bookmarksResults.innerHTML = '';
   for(var i = 0; i < bookmarks.length; i++){
@@ -65,4 +70,22 @@ function fetchBookmarks(){
                                   '</h3>'+
                                   '</div>';
   }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
 }
